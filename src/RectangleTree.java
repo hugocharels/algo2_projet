9fd6071861cfbfb70c170bdb2f378953ae196354
hugocharels <hugocharels@hotmail.com>
2023-03-15 23:30:13 +0100

    owo

---
+++ src/RectangleTree.java
@@ -0,0 +1,17 @@
+package src;
+
+public class RectangleTree {
+
+	public RectangleTree() {
+		System.out.println("New Tree");
+	}
+
+	public void chooseNode() {
+		System.out.println("Hello world!");
+	}
+
+	public void addLeaf() {
+		System.out.println("new Leaf added");
+	}
+
+}
