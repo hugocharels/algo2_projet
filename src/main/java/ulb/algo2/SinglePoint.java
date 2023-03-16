bc74a7b40579b43530405a9bd79cb2610e19e1b5
hugocharels <hugocharels@hotmail.com>
2023-03-16 19:28:00 +0100

    class node

--- src/main/java/ulb/algo2/SinglePoint.java
+++ src/main/java/ulb/algo2/SinglePoint.java
@@ -52,11 +52,11 @@ public class SinglePoint {
      */
     public static void main(String[] args) throws Exception {
         // display a data store file chooser dialog for shapefiles
-        String filename="../algo2_projet/data/sh_statbel_statistical_sectors_31370_20220101.shp/sh_statbel_statistical_sectors_31370_20220101.shp";
+        //String filename="../algo2_projet/data/sh_statbel_statistical_sectors_31370_20220101.shp/sh_statbel_statistical_sectors_31370_20220101.shp";
 
         //String filename="../algo2_projet/data/WB_countries_Admin0_10m/WB_countries_Admin0_10m.shp";
         
-        //String filename="../algo2_projet/data/communes-20220101-shp/communes-20220101.shp";
+        String filename="../algo2_projet/data/communes-20220101-shp/communes-20220101.shp";
         
         File file = new File(filename);
         if (!file.exists())
