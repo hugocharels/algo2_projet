0ed7783ab80b6a152c9d3c08a8bbad7e12fdc3ca
hugocharels <hugocharels@hotmail.com>
2023-03-25 21:39:00 +0100

    wowow linear done

--- src/main/java/ulb/algo2/rtrees/AbstractRectangleTree.java
+++ src/main/java/ulb/algo2/rtrees/AbstractRectangleTree.java
@@ -5,6 +5,7 @@ import org.locationtech.jts.geom.Polygon;
 import ulb.algo2.MBR;
 import ulb.algo2.node.*;
 
+import java.util.List;
 
 
 public abstract class AbstractRectangleTree {
@@ -12,6 +13,17 @@ public abstract class AbstractRectangleTree {
 	protected Node root;
 	protected final int N;
 
+
+	protected static class AbstractNodePair {
+		public AbstractNode n1;
+		public AbstractNode n2;
+
+		public AbstractNodePair() {}
+		public AbstractNodePair(AbstractNode n1, AbstractNode n2) {
+			this.n1 = n1;
+			this.n2 = n2;
+		}
+	}
 
 	// Constructor
 	public AbstractRectangleTree(int N) {
@@ -38,19 +50,57 @@ public abstract class AbstractRectangleTree {
 			node.addChild(new Leaf(node, new MBR(polygon), new LeafData(label, polygon)));
 		} else {
 			Node n = chooseNode(node, polygon);
-			Node new_node = addLeaf(n, label, polygon);
-			if (new_node != null) { node.addChild(new_node); }
+			Node newNode = addLeaf(n, label, polygon);
+			if (newNode != null) { node.addChild(newNode); }
 		}
 		node.expandMBR(new MBR(polygon));
 		if (node.maxChildrenReach(N)) { return split(node); }
 		return null;
 	}
 
-	public abstract Node split(Node node);
+	public Node split(Node node) {
+		// Get the two seeds
+		AbstractNodePair seeds = this.pickSeeds(node);
+		// Create a copy of the children
+		List<AbstractNode> children = new java.util.ArrayList<>(node.getChildren());
+		node.removeChildren();
+		// Sets mbr of nodes
+		node.setMBR(seeds.n1.getMBR());
+		Node newNode = new Node(node.getFather(), new MBR(seeds.n2.getMBR()));
+		this.pickNext(node, newNode, children);
+		return newNode;
+	}
 
+	protected AbstractNodePair pickSeeds(Node node) {
+		AbstractNodePair nodes = new AbstractNodePair(node.getChild(0), node.getChild(1));
+		MBR biggest = nodes.n1.getMBR().getUnion(nodes.n2.getMBR());
+		for (int i = 1; i < node.getChildren().size(); i++) {
+			for (int j = i + 1; j < node.getChildren().size(); j++) {
+				if (i==j) { continue; }
+				MBR union = node.getChild(i).getMBR().getUnion(node.getChild(j).getMBR());
+				if (union.getArea() > biggest.getArea()) {
+					nodes.n1 = node.getChild(i);
+					nodes.n2 = node.getChild(j);
+					biggest = union;
+				}
+			}
+		}
+		return nodes;
+	}
+
+	private void pickNext(Node n1, Node n2, List<AbstractNode> children) {
+		for (AbstractNode child : children) {
+			if (n1.getMBR().getExpansion(child.getMBR()) < n2.getMBR().getExpansion(child.getMBR())) {
+				n1.addChild(child);
+				n1.expandMBR(child.getMBR());
+			} else {
+				n2.addChild(child);
+				n2.expandMBR(child.getMBR());
+			}
+		}
+	}
 
 	// Setter
 	public void setRoot(Node root) { this.root = root; }
-
 
 }
--- src/main/java/ulb/algo2/rtrees/LinearRectangleTree.java
+++ src/main/java/ulb/algo2/rtrees/LinearRectangleTree.java
@@ -1,6 +1,12 @@
 package ulb.algo2.rtrees;
 
+import ulb.algo2.MBR;
+import ulb.algo2.node.AbstractNode;
 import ulb.algo2.node.Node;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 
 public class LinearRectangleTree extends AbstractRectangleTree {
@@ -8,12 +14,15 @@ public class LinearRectangleTree extends AbstractRectangleTree {
 	// Constructor
 	public LinearRectangleTree(int N) { super(N); }
 
-	// Split override
 	@Override
-	public Node split(Node node) {
-		// TODO implement
-		return null;
+	protected AbstractNodePair pickSeeds(Node node) {
+		List<AbstractNode> children = new java.util.ArrayList<>(node.getChildren());
+		Node newNode = new Node(null, null);
+		newNode.addChild(children.stream().min(Comparator.comparingDouble(n -> n.getMBR().getXMin())).orElse(null));
+		newNode.addChild(children.stream().min(Comparator.comparingDouble(n -> n.getMBR().getYMin())).orElse(null));
+		newNode.addChild(children.stream().max(Comparator.comparingDouble(n -> n.getMBR().getXMax())).orElse(null));
+		newNode.addChild(children.stream().max(Comparator.comparingDouble(n -> n.getMBR().getYMax())).orElse(null));
+		return super.pickSeeds(newNode);
 	}
-
 
 }
