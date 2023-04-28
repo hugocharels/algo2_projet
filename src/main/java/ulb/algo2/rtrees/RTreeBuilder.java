03e0d51a4c543ef2f5fb2eb05214a4d727085b0c
hugocharels <hugocharels@hotmail.com>
2023-04-28 12:54:18 +0200

    return value added

--- src/main/java/ulb/algo2/rtrees/RTreeBuilder.java
+++ src/main/java/ulb/algo2/rtrees/RTreeBuilder.java
@@ -13,19 +13,28 @@ import ulb.algo2.node.Node;
 
 public class RTreeBuilder {
 
-	public static void buildTree(AbstractRTree tree, SimpleFeatureCollection features) {
+
+	public static int buildTree(AbstractRTree tree, SimpleFeatureCollection features) {
+		buildTree(tree, features, Double.POSITIVE_INFINITY);
+	}
+
+	public static int buildTree(AbstractRTree tree, SimpleFeatureCollection features, double maxPolygon) {
 		ReferencedEnvelope bounds = features.getBounds();
 		Node root = new Node(null, new MBR(bounds.getMinX(), bounds.getMaxX(), bounds.getMinY(), bounds.getMaxY()));
 		tree.setRoot(root);
+		int polygonAdded = 0;
 		try (SimpleFeatureIterator iterator = features.features()) {
 			for (SimpleFeature feature = iterator.next(); iterator.hasNext(); feature = iterator.next()) {
 				MultiPolygon multiPolygon = (MultiPolygon) feature.getDefaultGeometry();
+				if (polygonAdded >= maxPolygon) break;
 				for (int i = 0; i < multiPolygon.getNumGeometries(); i++) {
+					if (polygonAdded >= maxPolygon) break;
 					Polygon polygon = (Polygon) multiPolygon.getGeometryN(i);
+					polygonAdded++;
 					tree.addLeaf(root, feature.getProperty("T_SEC_FR").getValue().toString(), polygon);
 				}
 			}
-		}
+		} return polygonAdded;
 	}
 
 }
